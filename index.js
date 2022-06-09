2c776cf5fee70327a7495b7b3068f0e87e91ab50
Margaret <wanjirundirangu43@gmail.com>
2022-06-09 12:52:15 +0300

    commit tree 5df990d912da135f766bf13d71204342def60bc0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee={   
+    name: "Boboh",   
+    streetAddress:"kenyattaAve"
+ }
+ const updateEmployeeWithKeyAndValue=(employee, key, value)=>{
+   const newEmployee = { ...employee };
+ 
+   newEmployee[key] = value;
+   let mergedEmployee = { ...employee, ...newEmployee };
+ 
+   return mergedEmployee;
+ };
+  
+ const destructivelyUpdateEmployeeWithKeyAndValue=(employee, key, value)=>{
+  employee[key]=value;
+   return employee;
+ }
+ const deleteFromEmployeeByKey=(employee, key, )=>{
+   const newEmployee={...employee};
+   delete newEmployee[key];
+   return newEmployee;
+ }
+ const destructivelyDeleteFromEmployeeByKey=(employee, key, )=>{
+   delete employee[key];
+   return employee;
+ }
